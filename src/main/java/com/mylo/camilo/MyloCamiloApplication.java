636663e13c776c8ff54b81d21c0b971a28f48d64
Camilo Mora <camiloalf23@gmail.com>
2025-01-15 19:23:45 -0500

    Archivos iniciales

---
+++ src/main/java/com/mylo/camilo/MyloCamiloApplication.java
@@ -0,0 +1,13 @@
+package com.mylo.camilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyloCamiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyloCamiloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mylo/camilo/MyloCamiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylo.camilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyloCamiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
